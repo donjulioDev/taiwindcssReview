4f74933856889b02b8c311db2f3d75dfe2ad97cc
donjulio.dev <julioejurado@icloud.com>
2023-06-01 16:12:51 -0600

    Tailwind CSS review

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+tailwind practice from  https://www.youtube.com/watch?v=lCxcTsOHrjo
+
+
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
---
+++ src/app/components/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Main = () => {
+  return (
+    <div className='min-h-screen grid place-content-center' >
+        <div className='bg-emerald-500 w-52 h-52 rounded-full shadow-2xl grid place-content-center'>
+
+        <div className='bg-teal-200 w-32 h-32 rounded-full grid place-content-center'>
+            <div className="bg-red-500 w-16 h-16 rounded-full" > </div>
+        </div>
+        </div>
+    </div>
+  )
+}
